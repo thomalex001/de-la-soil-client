28af2cc805d41c55d0bee7ccc3abfe12a88c3cba
Alex Thomas MacBook <alexthomas@gmail.local>
2025-03-21 01:15:29 +0000

    register

---
+++ src/lib/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { AUTH } from './auth';
+
+const getHeaders = () => ({
+  headers: { Authorization: `Bearer ${AUTH.getToken()}` }
+});
+
+const ENDPOINTS = {
+  register: `${process.env.REACT_APP_BASE_URL}/api/auth/register/`,
+  login: `${process.env.REACT_APP_BASE_URL}/api/auth/login/`,
+  allProducts: `${process.env.REACT_APP_BASE_URL}/api/products/`,
+  createUserCart: `${process.env.REACT_APP_BASE_URL}/api/cart/`,
+  cloudinary: `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
+  // search: (query) => `/api/books/search?q=${query}`,
+  // updateUserCart: (cartId) => `/api/cart/${cartId}/`,
+  userCart: (cartId) => `${process.env.REACT_APP_BASE_URL}/api/cart/${cartId}/`,
+  // deleteUserCart: (cartId) => `${process.env.REACT_APP_BASE_URL}/api/cart/${cartId}/`
+};
+
+const GET = (endpoint, headers) =>
+  headers ? axios.get(endpoint, headers) : axios.get(endpoint);
+const POST = (endpoint, body, headers) =>
+  headers ? axios.post(endpoint, body, headers) : axios.post(endpoint, body);
+const PUT = (endpoint, body, headers) => axios.put(endpoint, body, headers);
+const DELETE = (endpoint, headers) => axios.delete(endpoint, headers);
+
+export const API = { GET, POST, PUT, DELETE, ENDPOINTS, getHeaders };
